469ea3821491e89457da2a5b6f4af365029ceab2
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-24 15:54:10 +0200

    refactor(Model): make methods void

--- src/todoapp/Model.java
+++ src/todoapp/Model.java
@@ -12,23 +12,20 @@ class Model {
     this.tasks.add(new Task("Walk the dog"));
   }
 
-  List<Task> add(String name) {
+  void add(String name) {
     this.tasks.add(new Task(name));
-    return getTasks();
   }
 
-  List<Task> remove(String index) {
+  void remove(String index) {
     this.tasks.remove(Integer.parseInt(index) - 1);
-    return getTasks();
   }
 
-  List<Task> complete(String index) {
+  void complete(String index) {
     for (int i = 0; i < this.tasks.size(); i++) {
       if (i + 1 == Integer.parseInt(index)) {
         tasks.get(i).complete();
       }
     }
-    return getTasks();
   }
 
   List<Task> getTasks() {
